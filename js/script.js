26a45363e67f77a0c0cb9f6770636801d2cb1ebb
RCH <robertochirila@yahoo.com>
2018-01-12 16:10:05 +0200

    "Added responsive navigation"

--- js/script.js
+++ js/script.js
@@ -148,6 +148,19 @@ $(document).ready(function () {
     }, {
         offset: '100%'
     });
+    /* Navigation icon */
+    $('.js--nav-icon').click(function () {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        nav.slideToggle(200);
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.removeClass('ion-navicon-round');
+            icon.addClass('ion-close-round');
+        } else {
+            icon.removeClass('ion-close-round');
+            icon.addClass('ion-navicon-round');
+        }
+    });
 
 
 });
